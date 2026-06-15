6ccfcd6458d1cb3ae8df8cb13b8f8ecb1bf31fd6
hohung170502 <2000003074@nttu.edu.vn>
2026-06-15 15:46:35 +0700

    Add upload cloudinary

---
+++ Migrations/20260615084607_AddImagePublicIdToBrand.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pharmacy_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagePublicIdToBrand : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePublicId",
+                table: "Brands",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePublicId",
+                table: "Brands");
+        }
+    }
+}
